2d1f66ef1bf2c49bf89b85540c48949f1f2bfad5
swaroop1199 <hiremathswaroop@gmail.com>
2023-08-16 11:55:22 +0530

    Add project files.

---
+++ Development/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Threading;
+using System.Text;
+
+namespace Development
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            add_button.Click += Add_Click;
+
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
